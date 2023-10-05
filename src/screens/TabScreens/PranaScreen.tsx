cae926cddd9950ee9ec0fed1e4375051fbc4b66e
Lorenzo Torres <lorenzoajt@gmail.com>
2023-10-05 11:36:52 -0600

    add single video to prana

--- src/screens/TabScreens/PranaScreen.tsx
+++ src/screens/TabScreens/PranaScreen.tsx
@@ -1,14 +1,41 @@
-import React from 'react'
-import { Text, View } from 'react-native'
-import Icon from 'react-native-vector-icons/Ionicons';
-import { colors } from '../../theme/appTheme'
+import React, { useState } from 'react';
+import { View, StyleSheet, TouchableWithoutFeedback, FlatList, Dimensions } from 'react-native';
+import Video from 'react-native-video';
+import pranas from '../../data/pranas';
 
 export const PranaScreen = () => {
+
+  const [paused, setPaused] = useState(false)
+  const onPlayPlayPausePress = () => {
+    setPaused(!paused)
+  }
+
   return (
-    <View>
-        <Text>
-          <Icon name='airplane-outline' size={80} color={colors.primary}/>
-        </Text>
+    <View style={styles.container}>
+      <TouchableWithoutFeedback onPress={onPlayPlayPausePress}>
+        <Video
+          source={{ uri: 'https://res.cloudinary.com/doiyktiuw/video/upload/v1696525925/Nirvana/f2igaepguucd152jdrlp.mp4' }}
+          style={styles.video}
+          resizeMode="cover"
+          paused={paused}
+          repeat        
+          onError={(error)=> console.log(error)}
+        />
+        
+      </TouchableWithoutFeedback>
     </View>
   )
 }
+
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  video: {
+    flex: 1,
+  },
+});
+
